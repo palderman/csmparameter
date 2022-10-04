9971082c5f9dadba07c210b9dc4bab1065d592c8
Phillip Alderman <phillip.d.alderman@gmail.com>
2022-10-04 16:30:05 -0500

    Adding a new function ptrans_create() to create a parameter transformation function

--- R/ptrans_create.R
+++ R/ptrans_create.R
@@ -1,24 +1,35 @@
-# ptrans_arg_list <- function(expr){
-#   arg_list <- all.vars(substitute(expr))
-#   return(arg_list)
-# }
-
-ptrans_get_ind <- function(pnames, expr){
-  ind <- match(all.vars(substitute(expr)), pnames)
-  return(ind)
+ptrans_internal <- function(arg_list, body){
+  paste0("function(",
+           paste(arg_list, collapse = ", "),
+         "){",
+            body,
+         "}"
+  )
 }
 
-#' Create a parameter transformation function
+ptrans_fun <- function(arg_list, body, pind){
+  internal_fun <- ptrans_internal(arg_list, body)
+  new_f <- eval(parse(text = paste0("function(prm){",
+                                      "do.call(",
+                                        internal_fun,
+                                        ",",
+                                        "as.list(prm[c(",
+                                          paste(pind, collapse = ", "),
+                                        ")])",
+                                      ")",
+                                    "}"
+                                    )))
+  return(new_f)
+}
+
+#' Create a parameter transformation
 #'
 #' @export
 #'
-ptrans_create <- function(expr){
+ptrans_create <- function(pnames, expr){
   arg_list <- all.vars(substitute(expr))
-  new_f <- eval(parse(text = paste0("function(",
-                                    paste(arg_list, collapse = ", ")
-                                    ,
-                                    "){\n",
-                                    deparse(substitute(expr)),
-                                    "\n}\n")))
-  return(new_f)
+  body <- deparse(substitute(expr))
+  pind <- match(arg_list, pnames)
+  fun <- ptrans_fun(arg_list, body, pind)
+  return(fun)
 }
--- tests/testthat/test-ptrans_tbl.R
+++ tests/testthat/test-ptrans_tbl.R
@@ -1,33 +1,28 @@
-# test_that("ptrans_arg_list",{
-#
-#   test <- function(expr){
-#     parametR:::ptrans_arg_list(expr)
-#   }
-#
-#   expect_identical(
-#     test(p1 + p2 + p3),
-#     c("p1", "p2", "p3")
-#   )
-# })
+test_that("ptrans_internal",{
+
+  expect_identical(parametR:::ptrans_internal(c("p1", "p2", "p3"), "p1 + p2 + p3"),
+                   "function(p1, p2, p3){p1 + p2 + p3}")
+
+  expect_identical(parametR:::ptrans_internal(c("p1", "p2", "p3"), "p1 + p2 + p3/p2"),
+                   "function(p1, p2, p3){p1 + p2 + p3/p2}")
+
+})
 
 test_that("ptrans_create",{
-
-  expect_identical(deparse(parametR::ptrans_create(expr = p1 + p2 + p3)),
-                   deparse(function(p1, p2, p3){
-                     p1 + p2 + p3
-                   }))
-
-  expect_identical(deparse(parametR::ptrans_create(expr = p1 + p2 + p3/p2)),
-                   deparse(function(p1, p2, p3){
-                     p1 + p2 + p3/p2
-                   }))
-
+  expect_identical(deparse(
+                     parametR::ptrans_create(c("p1", "p3", "p2"), p1+p2+p3/p2)
+                   ),
+                   deparse(
+                     function(prm){
+                       do.call(
+                         function(p1, p2, p3){
+                           p1 + p2 + p3/p2
+                         },
+                         as.list(prm[c(1, 3, 2)])
+                       )
+                     }
+                   )
+  )
 })
 
-test_that("ptrans_get_ind",{
 
-  expect_identical(parametR:::ptrans_get_ind(c("p1", "p3", "p2"),
-                                             p1 + p2 + p3/p2),
-                   c(1L, 3L, 2L))
-
-})
