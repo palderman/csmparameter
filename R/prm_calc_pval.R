628e53e199362d1fac53e7903b6d92bace5d7fec
Phillip D. Alderman <phillip.alderman@okstate.edu>
2022-11-08 16:18:44 -0600

    Adding function to calculate transformed parameter values from input parameter values

---
+++ R/prm_calc_pval.R
@@ -0,0 +1,36 @@
+#' Calculate parameter values (regular and transformed)
+#'
+#' @export
+#'
+#' @importFrom magrittr "%>%"
+#' @importFrom dplyr filter pull
+#' @importFrom purrr map_lgl map
+#'
+prm_calc_pval <- function(prm_tbl, pval_in = NULL, n = 1){
+
+  ptransform_ind <- prm_tbl %>%
+    pull(ptransform) %>%
+    map_lgl(~{!is.null(.x)})
+
+  if(is.null(pval_in)){
+    pval_in <- prm_tbl %>%
+      prm_sample_prior(n = n)
+  }else if(!is.matrix(pval_in)){
+    pval_in <- matrix(pval_in, nrow = 1)
+  }
+
+  pval_transformed <- prm_tbl %>%
+    filter(ptransform_ind) %>%
+    pull(ptransform) %>%
+    map(~apply(pval_in, 1, .x)) %>%
+    do.call(cbind, .)
+
+  pval_out <- matrix(0.,
+                     nrow = n,
+                     ncol = nrow(prm_tbl))
+
+  pval_out[, !ptransform_ind] <- pval_in
+  pval_out[, ptransform_ind] <- pval_transformed
+
+  return(pval_out)
+}
