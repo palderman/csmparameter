d051becb0991fac69bfaca24744912130bd01b94
Phillip Alderman <phillip.d.alderman@gmail.com>
2022-11-09 13:32:42 -0600

    Updating lp_fun() and associated tests:  - Pulling calculation of prior density into a separate internal function lp_prior_density()  - Adding code to calculate log likelihood  - Adding tests for prior log probability and log likelihood

--- R/lp_fun.R
+++ R/lp_fun.R
@@ -1,32 +1,48 @@
-#' @export
 #'
 #' @importFrom magrittr "%>%"
 #' @importFrom tibble add_column
-#' @importFrom dplyr filter mutate
+#' @importFrom dplyr filter mutate rowwise pull
 #' @importFrom purrr map_dbl map_lgl
 #'
-lp_fun <- function(obs_tbl, sim_tbl, prm_tbl, pval){
-
-  prior_lp <- prm_tbl %>%
-    filter(map_lgl(pdensity, ~{!is.null(.x)})) %>%
+lp_prior_density <- function(prm_tbl, pval){
+  prm_tbl %>%
     add_column(pval = pval) %>%
+    filter(map_lgl(pdensity, ~{!is.null(.x)})) %>%
     rowwise() %>%
     mutate(prior_lp = pdensity(pval)) %>%
     pull(prior_lp) %>%
     sum()
+}
 
-  lp <- prior_lp
+#' @export
+#'
+#' @importFrom magrittr "%>%"
+#' @importFrom tibble add_column
+#' @importFrom dplyr full_join mutate
+#'
+lp_fun <- function(obs_tbl, sim_tbl, prm_tbl, pval){
 
-  # if(check_sim_data(sim_tbl)){
-  #   lp <- obs_tbl %>%
-  #     full_join(sim_tbl) %>%
-  #     mutate(sq_err=(obs-sim)^2) %>%
-  #     summarize(rSSE=sum(sq_err)/mean(obs)) %>%
-  #     summarize(rSSE = sum(rSSE)) %>%
-  #     pull(rSSE)
-  # }else{
-  #   lp <- .Machine$double.xmax
-  # }
+  if(check_sim_data(sim_tbl)){
+
+    # Calculate prior density
+    prior_lp <- lp_prior_density(prm_tbl, pval)
+
+    log_likelihood <- obs_tbl %>%
+      # Combine observed and simulated data
+      full_join(sim_tbl) %>%
+      # Extract variance term from from pval vector
+      mutate(obs_sigma = pval[lp_sigma_ind]) %>%
+      # Calculate log-likelihood assuming errors normally distributed
+      with(dnorm(obs, mean = sim, sd = obs_sigma, log = TRUE)) %>%
+      sum()
+
+    lp <- prior_lp + log_likelihood
+
+  }else{
+
+    lp <- -.Machine$double.xmax
+
+  }
 
   return(lp)
 
--- tests/testthat/test-lp_fun.R
+++ tests/testthat/test-lp_fun.R
@@ -1,12 +1,27 @@
-test_that("lp for pmu", {
 
-  obs_tbl <- NULL
+  obs_tbl <- tidyr::unnest(
+    dplyr::mutate(
+      dplyr::group_by(
+        tibble::tribble(
+          ~EXPERIMENT, ~TRNO,                    ~DATE, ~variable, ~obs, ~lp_sigma_ind,
+          "INST0001",      1, as.POSIXct("0001-01-01"),    "HWAM", 4000,             4,
+          "INST0001",      1, as.POSIXct("0001-01-01"),    "HIAM", 0.35,             5,
+          "INST0001",      2, as.POSIXct("0001-01-01"),    "HWAM", 4000,             4,
+          "INST0001",      2, as.POSIXct("0001-01-01"),    "HIAM", 0.35,             5
+        ),
+        EXPERIMENT, TRNO, DATE, variable
+      ),
+      obs = list(rnorm(4, mean = obs, sd = obs*.13))
+    ),
+    obs
+  )
 
-  sim_tbl <- NULL
+  sim_tbl <- dplyr::summarize(obs_tbl, sim = mean(obs))
 
   pmu <- c(25, 32, 3.8)
 
-  prm_tbl <- parametR::prm_create(pname = c("G1", "G2", "G3"),
+  prm_tbl <- parametR::prm_add_variances(
+    parametR::prm_create(pname = c("G1", "G2", "G3"),
                                   pmin = c(16, 1.2, 0.5),
                                   pmax = c(36, 43, 23),
                                   pmu = c(25, 32, 3.8),
@@ -14,16 +29,45 @@ test_that("lp for pmu", {
                                   pdist = "normal",
                                   pfile = "WHCER048.CUL",
                                   ptier = NA,
-                                  pkey = "IB0001")
+                                  pkey = "IB0001"),
+    dplyr::group_by(obs_tbl, variable),
+    psigma = c(250, 0.025),
+    variable = c("HWAM", "HIAM")
+  )
 
-  prior_lp <- 0
-  for(i in seq_along(prm_tbl$pdensity)){
-    prior_lp <- prior_lp + prm_tbl$pdensity[[i]](pmu[i])
-  }
+  pval <- c(pmu, 250, 0.025)
+  prior_lp <- sum(
+    sapply(
+      seq_along(prm_tbl$pdensity),
+      function(i) prm_tbl$pdensity[[i]](pval[i])
+      )
+    )
+
+  sd_tmp <- sapply(obs_tbl$variable, switch, "HWAM" = 250, "HIAM" = 0.025)
+
+  log_likelihood <- with(dplyr::full_join(obs_tbl, sim_tbl),
+                         sum(
+                           dnorm(obs,
+                                 mean = sim,
+                                 sd = sd_tmp,
+                                 log = TRUE)
+                           )
+                         )
+
+  lp <- prior_lp + log_likelihood
+
+test_that("lp_prior_density()", {
+  expect_identical(
+    parametR:::lp_prior_density(prm_tbl, pval),
+    prior_lp
+  )
+})
+
+test_that("lp_fun()", {
 
   expect_identical(
-    parametR::lp_fun(obs_tbl, sim_tbl, prm_tbl, pmu),
-    prior_lp
+    parametR::lp_fun(obs_tbl, sim_tbl, prm_tbl, pval),
+    lp
   )
 
 })
