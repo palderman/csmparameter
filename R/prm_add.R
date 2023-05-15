3a589f7b3edd6d370e9158d3595d1385401843d2
Phillip D. Alderman <phillip.alderman@okstate.edu>
2023-05-15 08:53:28 -0500

    Removing get_p* and rename add_dssat_prm() to prm_add()

--- R/prm_add.R
+++ R/prm_add.R
@@ -3,9 +3,9 @@
 #'
 #' @export
 #'
-add_dssat_prm <- function(.prm_tbl, ...){
+prm_add <- function(.prm_tbl, ...){
 
-  .prm_tbl <- create_dssat_prm(...) %>%
+  .prm_tbl <- prm_create(...) %>%
     full_join(.prm_tbl,.) %>%
     as_dssat_prm_tbl()
 
