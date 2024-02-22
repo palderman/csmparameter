5b28c00447d038cee42ccaeecc1d27eb8eb266c2
Phillip D. Alderman <phillip.alderman@okstate.edu>
2024-02-22 12:25:00 -0600

    Bugfix for prm_create_expmt_tbl() that allows specification of data type for each file X

--- R/prm_create_expmt_tbl.R
+++ R/prm_create_expmt_tbl.R
@@ -8,16 +8,18 @@ prm_create_expmt_tbl <- function(filex_name, trno = NULL, data_types = NULL,
                                  rewrite_filex = FALSE){
   if(is.null(trno)){
     filex_trno_tbl <- tibble(filex_name = filex_name,
-                             trno = map(filex_name,~{NULL}))
+                             trno = map(filex_name,~{NULL}),
+                             data_types = data_types)
   }else{
     filex_trno_tbl <- tibble(filex_name = filex_name,
-                             trno = trno)
+                             trno = trno,
+                             data_types = data_types)
   }
   expmt_tbl <- filex_trno_tbl |>
     group_by(filex_name) |>
     group_map(~create_expmt(filex_name = .y$filex_name,
                             trno = .x$trno[[1]],
-                            data_types = data_types,
+                            data_types = .x$data_types[[1]],
                             rewrite_filex = rewrite_filex)) |>
     bind_rows()
   return(expmt_tbl)
