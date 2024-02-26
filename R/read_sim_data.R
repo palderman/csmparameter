06ddb96d9a42561f6d0457db0e5fab4e9dd0a718
Phillip D. Alderman <phillip.alderman@okstate.edu>
2024-02-26 13:06:11 -0600

    Adding condition in read_sim_data() to skip reading simulated output when sim_template has zero rows in it

--- R/read_sim_data.R
+++ R/read_sim_data.R
@@ -9,64 +9,71 @@
 #'
 read_sim_data <- function(run_tbl){
 
-  run_expmt <- run_tbl |>
-    select(sim_template) |>
-    unnest(sim_template) |>
-    group_by(EXPERIMENT,TRNO) |>
-    summarize() |>
-    ungroup() |>
-    mutate(RUN = 1:n(),
-           RUNNO = RUN)
+  if(nrow(run_tbl$sim_template[[1]]) > 0){
 
-  all_cols <- run_tbl |>
-    ungroup() |>
-    select(out_tbl) |>
-    unnest(out_tbl) |>
-    pull(col_names)
+    run_expmt <- run_tbl |>
+      select(sim_template) |>
+      unnest(sim_template) |>
+      group_by(EXPERIMENT,TRNO) |>
+      summarize() |>
+      ungroup() |>
+      mutate(RUN = 1:n(),
+             RUNNO = RUN)
 
-  out_tbl <- run_tbl |>
-    ungroup() |>
-    select(out_tbl) |>
-    unnest(out_tbl)
+    all_cols <- run_tbl |>
+      ungroup() |>
+      select(out_tbl) |>
+      unnest(out_tbl) |>
+      pull(col_names)
 
-  # Reject if any output file is missing
-  # This assumes that if any output file is missing the simulation failed
-  if(all(file.exists(out_tbl$file_name))){
-    out <- out_tbl |>
-      group_by(file_name) |>
-      group_map(~{
-        # read_output(.y$file_name,read_only = c('TRNO','DATE','RUN','RUNNO',.x$col_names)) |>
-        read_output(.y$file_name) |>
-        (\(.x){
-          if( ! 'DATE' %in% names(.x)){
-            .x <- add_column(.x, DATE = as.POSIXct('0001001',format='%Y%j',tz='UTC'))
-          }
-          .x
-          })() |>
-          filter(TRNO %in% run_tbl$sim_template[[1]]$TRNO &
-                 DATE %in% run_tbl$sim_template[[1]]$DATE) |>
-          rename_with(~str_replace(.,'RUNNO','RUN')) |>
-          select(-matches("(EXPERIMENT)|(MODEL)")) |>
-          full_join(run_expmt) |>
-          select(any_of(c("EXPERIMENT", "TRNO", "DATE", all_cols))) |>
-          pivot_longer(names_to = "variable",
-                       values_to = "sim",
-                       cols = any_of(all_cols))
-      }) |>
-      bind_rows() |>
-      (\(.x) left_join(run_tbl$sim_template[[1]], .x)
-       )() |>
-      mutate(sim = ifelse(str_detect(variable,'DAT$'),
-                          as.numeric(difftime(as.POSIXct(sim,tz='UTC',origin='1970-01-01'),
-                                              PDATE,
-                                              units="days")),
-                          sim)) |>
-      select(-PDATE)
-    }else{
-      out <- run_tbl$sim_template[[1]] |>
-        mutate(sim = NA_real_) |>
+    out_tbl <- run_tbl |>
+      ungroup() |>
+      select(out_tbl) |>
+      unnest(out_tbl)
+
+    # Reject if any output file is missing
+    # This assumes that if any output file is missing the simulation failed
+    if(all(file.exists(out_tbl$file_name))){
+      out <- out_tbl |>
+        group_by(file_name) |>
+        group_map(~{
+          # read_output(.y$file_name,read_only = c('TRNO','DATE','RUN','RUNNO',.x$col_names)) |>
+          read_output(.y$file_name) |>
+          (\(.x){
+            if( ! 'DATE' %in% names(.x)){
+              .x <- add_column(.x, DATE = as.POSIXct('0001001',format='%Y%j',tz='UTC'))
+            }
+            .x
+            })() |>
+            filter(TRNO %in% run_tbl$sim_template[[1]]$TRNO &
+                   DATE %in% run_tbl$sim_template[[1]]$DATE) |>
+            rename_with(~str_replace(.,'RUNNO','RUN')) |>
+            select(-matches("(EXPERIMENT)|(MODEL)")) |>
+            full_join(run_expmt) |>
+            select(any_of(c("EXPERIMENT", "TRNO", "DATE", all_cols))) |>
+            pivot_longer(names_to = "variable",
+                         values_to = "sim",
+                         cols = any_of(all_cols))
+        }) |>
+        bind_rows() |>
+        (\(.x) left_join(run_tbl$sim_template[[1]], .x)
+         )() |>
+        mutate(sim = ifelse(str_detect(variable,'DAT$'),
+                            as.numeric(difftime(as.POSIXct(sim,tz='UTC',origin='1970-01-01'),
+                                                PDATE,
+                                                units="days")),
+                            sim)) |>
         select(-PDATE)
-    }
+      }else{
+        out <- run_tbl$sim_template[[1]] |>
+          mutate(sim = NA_real_) |>
+          select(-PDATE)
+      }
+  }else{
+    out <- run_tbl$sim_template[[1]] |>
+      mutate(sim = vector("numeric")) |>
+      select(-PDATE)
+  }
 
   return(out)
 
