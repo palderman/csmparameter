4b2ab03d5eaeae462c6b9f34ca294eb6ce961a1e
Phillip D. Alderman <phillip.alderman@okstate.edu>
2024-02-26 13:25:38 -0600

    Bugfix for missing variable "." in check_sim_data()

--- R/check_sim_data.R
+++ R/check_sim_data.R
@@ -6,11 +6,12 @@ check_sim_data <- function(obs_sim){
     warn_out <- obs_sim |>
       filter(is.na(sim)) |>
       (\(.x) capture.output(print(.x)))() |>
-      c(paste0("Missing values were present in simulated output.",
+      (\(.x) c(
+        paste0("Missing values were present in simulated output.",
                " The objective function value will be set to ",
                .Machine$double.xmax,"."),
         "The following observations were missing:",
-        .) |>
+        .x))() |>
       str_c('\n')
     warning(warn_out)
     ok <- FALSE
