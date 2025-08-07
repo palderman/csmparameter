b1ffea1557940663bb65d942d3f4a69a29c3edd1
Phillip D. Alderman <phillip.alderman@okstate.edu>
2025-08-07 13:47:10 -0500

    Removing tidyverse dependencies from prm_get_model_outputs()

--- R/prm_get_model_outputs.R
+++ R/prm_get_model_outputs.R
@@ -2,9 +2,6 @@
 #'
 #' @export
 #'
-#' @importFrom dplyr  is_grouped_df group_by select group_map
-#' @importFrom tidyr crossing unnest
-#'
 #' @param prm_est a list object as returned by prm_create_prm_est()
 #'
 #' @param pvals a vector of parameter values for which to generate model outputs
@@ -18,27 +15,33 @@ prm_get_model_outputs <- function(prm_est, pvals){
     prm_write_inputs(prm_est$input_df, prm_est$prm_df, pvals)
 
     outputs <- run_df |>
-      group_map(~{crossing(.y,.x) |>
-          select(filex_trno) |>
-          unnest(filex_trno) |>
-          (\(.x) DSSAT::write_dssbatch(x = .x$filex_name,
-                                       trtno = .x$trno)
-          )()
-        dssat_out <- tryCatch({
-          system(.x$dssat_call, intern = TRUE)
-        },error = function(e){NULL})
-        if(!is.null(dssat_out)){
-          suppressMessages(prm_read_sim(.x))
-        }else{
-          .x |>
-            select(sim_template) |>
-            unnest(sim_template) |>
-            group_by(EXPERIMENT,TRNO) |>
-            summarize() |>
-            mutate(sim = NA_real_)
-        }
-      }) |>
-      bind_rows()
+      by(INDICES = run_df$group,
+         \(.g){
+           # Write batch file
+           .g[["filex_trno"]] |>
+             do.call(rbind, args = _) |>
+             with({DSSAT::write_dssbatch(x = filex_name,
+                                         trtno = trno)})
+           # Call model
+           dssat_out <- tryCatch({
+             .g[["model_call"]] |>
+               unique() |>
+               system(intern = TRUE)},
+             error = function(e){NULL})
+           # Read outputs
+           if(!is.null(dssat_out)){
+             suppressMessages(prm_read_sim(.g))
+           }else{
+             .g[["sim_template"]] |>
+               do.call(rbind, args = _) |>
+               subset(select = c("EXPERIMENT", "TRNO")) |>
+               unique() |>
+               within({
+                 sim = NA_real_
+               })
+           }
+         }, SIMPLIFY = FALSE) |>
+      do.call(rbind, args = _)
   }else{
     paste0("prm_get_model_outputs() not defined for model_type = \"",
            prm_est$model_type,"\"") |>
