701e46ee2076b117ce83d5cd7eb4900ccf364387
Phillip D. Alderman <phillip.alderman@okstate.edu>
2025-08-07 07:19:26 -0500

    Removing tidyverse dependencies from prm_create_prm_est()

--- R/prm_create_prm_est.R
+++ R/prm_create_prm_est.R
@@ -2,49 +2,78 @@
 #'
 #' @export
 #'
-#' @importFrom dplyr summarize bind_rows full_join pull
-#' @importFrom tibble tibble
-#' @importFrom purrr map
-#' @importFrom tidyr unnest
-#'
 #' @param expmt_df a data frame of experiment definitions as created by
-#'   \link[csmparameter]{prm_create_expmt_df}
+#'   \link{prm_create_expmt_df}
+#'
 #' @param inp_df a data frame of input definitions as created by
 #'   \link{prm_create_inp_df}
+#'
 #' @param prm_df a data frame
 #'
 prm_create_prm_est <- function(expmt_df, inp_df,
                                prm_df, obj_fun,
                                model_type = "DSSAT-CSM",
-                               dssat_call){
+                               model_call){
 
-  if(missing(dssat_call)){
+  if(missing(model_call)){
     dssat_exec <- getOption("DSSAT.CSM")
-    if(is.null(dssat_exec)) stop("Please include a value for the dssat_call argument or set the executable using options(DSSAT.CSM = \"<path to executable>\")")
+    if(is.null(dssat_exec)) stop("Please include a value for the model_call argument or set the executable using options(DSSAT.CSM = \"<path to executable>\")")
     version <- DSSAT:::get_dssat_version()
     file_name <- paste0("DSSBatch.V", version)
-    dssat_call <- dssat_exec |>
+    model_call <- dssat_exec |>
       paste("B", file_name, sep = " ")
   }
 
-  run_df <- expmt_df |>
-    summarize(filex_trno = tibble(filex_name = filex_name,trno = trno) |>
-                unnest(cols=trno) |>
-                list(),
-              sim_template = sim_template |>
-                map(~full_join(.$data_template[[1]],.$pdate[[1]])) |>
-                reduce(full_join) |>
-                list(),
-              out_df = out_df |>
-                map(~unnest(.,cols=col_names)) |>
-                reduce(full_join) |>
-                list(),
-              dssat_call = dssat_call,
-              .groups = "keep")
+  if("group" %in% colnames(expmt_df)){
+    run_df <-
+      expmt_df[["group"]] |>
+      unique() |>
+      data.frame(group = _)
+  }else{
+    run_df <-
+      data.frame(group = 1)
+    expmt_df[["group"]] <- 1
+  }
 
-  obs_df <- expmt_df |>
-    pull(obs_df) |>
-    bind_rows()
+  run_df[["filex_trno"]] <-
+    by(expmt_df,
+       expmt_df[["group"]],
+       \(.x) list(data.frame(filex_name = .x$filex_name,
+                             trno = .x$trno)),
+       simplify = FALSE)
+
+  run_df[["sim_template"]] <-
+    by(expmt_df, expmt_df$group,
+       \(.df){
+           lapply(.df$sim_template,
+                  FUN = unlist,
+                  recursive = FALSE) |>
+           lapply(\(.x) merge(.x$data_template,
+                              .x$pdate,
+                              all = TRUE)) |>
+           Reduce(\(.x, .y) merge(.x, .y, all = TRUE),
+                  x = _)
+       }, simplify = FALSE)
+
+
+  run_df[["out_df"]] <-
+    by(expmt_df, expmt_df$group,
+       \(.df){
+         .df[["out_df"]] |>
+           lapply(\(.x) with(.x,
+                             setNames(col_names, file_name) |>
+                             stack() |>
+                             rev() |>
+                             setNames(names(.x)))) |>
+           Reduce(\(.x, .y) merge(.x, .y, all = TRUE),
+                  x = _)
+       }, simplify = FALSE)
+
+  run_df[["model_call"]] <- model_call
+
+  obs_df <-
+    expmt_df[["obs_df"]] |>
+    do.call(rbind, args = _)
 
   prm_est <- list(expmt_df = expmt_df,
                  input_df = input_df,
