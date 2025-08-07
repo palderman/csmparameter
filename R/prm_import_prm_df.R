513d7cd7c5063bbe54f31d01576a314a8b4de9ce
Phillip D. Alderman <phillip.alderman@okstate.edu>
2025-08-07 10:21:04 -0500

    Removing tidyverse dependencies from prm_import_prm_df()

--- R/prm_import_prm_df.R
+++ R/prm_import_prm_df.R
@@ -1,122 +1,106 @@
 #'
-#' @importFrom readr read_csv
-#' @importFrom dplyr  pull
-#'
 #' @export
 #'
 prm_import_prm_df <- function(file_name){
 
-  prm_df_csv <- read_csv(file_name)
+  prm_df_csv <- read.csv(file_name)
 
-  pname <- prm_df_csv |>
-    pull(pname) |>
+  pname <- prm_df_csv[["pname"]] |>
     as.character()
 
-  pfile <- prm_df_csv |>
-    pull(pfile) |>
+  pfile <- prm_df_csv[["pfile"]] |>
     as.character()
 
   if('pmin' %in% colnames(prm_df_csv)){
-    pmin <- prm_df_csv |>
-      pull(pmin) |>
+    pmin <- prm_df_csv[["pmin"]] |>
       as.numeric()
   }else{
     pmin <- -Inf
   }
 
   if('pmax' %in% colnames(prm_df_csv)){
-    pmax <- prm_df_csv |>
-      pull(pmax) |>
+    pmax <- prm_df_csv[["pmax"]] |>
       as.numeric()
   }else{
     pmax <- Inf
   }
 
   if('pmu' %in% colnames(prm_df_csv)){
-    pmu <- prm_df_csv |>
-      pull(pmu) |>
+    pmu <- prm_df_csv[["pmu"]] |>
       as.numeric()
   }else{
     pmu <- NA
   }
 
   if('psigma' %in% colnames(prm_df_csv)){
-    psigma <- prm_df_csv |>
-      pull(psigma) |>
+    psigma <- prm_df_csv[["psigma"]] |>
       as.numeric()
   }else{
     psigma <- NA
   }
 
   if('pdist' %in% colnames(prm_df_csv)){
-    pdist <- prm_df_csv |>
-      pull(pdist) |>
+    pdist <- prm_df_csv[["pdist"]] |>
       as.character()
   }else{
     pdist <- "unif"
   }
 
   if('ptier' %in% colnames(prm_df_csv)){
-    ptier <- prm_df_csv |>
-      pull(ptier) |>
+    ptier <- prm_df_csv[["ptier"]] |>
       as.character()
   }else{
-    ptier <- as.character(NA)
+    ptier <- NA_character_
   }
 
   if('pkey' %in% colnames(prm_df_csv)){
-    pkey <- prm_df_csv |>
-      pull(pkey) |>
+    pkey <- prm_df_csv[["pkey"]] |>
       as.character()
   }else{
-    pkey <- as.character(NA)
+    pkey <- NA_character_
   }
 
   if('plev' %in% colnames(prm_df_csv)){
-    plev <- prm_df_csv |>
-      pull(plev) |>
+    plev <- prm_df_csv[["plev"]] |>
       as.numeric()
   }else{
-    plev <- as.numeric(NA)
+    plev <- NA_real_
   }
 
   if('pind' %in% colnames(prm_df_csv)){
-    pind <- prm_df_csv |>
-      pull(pind) |>
+    pind <- prm_df_csv[["pind"]] |>
       as.numeric()
   }else{
-    pind <- as.numeric(NA)
+    pind <- NA_real_
   }
 
   if('pnum' %in% colnames(prm_df_csv)){
-    pnum <- prm_df_csv |>
-      pull(pnum) |>
+    pnum <- prm_df_csv[["pnum"]] |>
       as.numeric()
   }else{
     pnum <- NULL
   }
 
   if('pwt' %in% colnames(prm_df_csv)){
-    pwt <- prm_df_csv |>
-      pull(pwt) |>
+    pwt <- prm_df_csv[["pwt"]] |>
       as.numeric()
   }else{
     pwt <- NULL
   }
 
-  prm_df <- prm_create(pname = pname,
-                        pfile = pfile,
-                        pmin = pmin,
-                        pmax = pmax,
-                        pmu = pmu,
-                        psigma = psigma,
-                        pdist = pdist,
-                        ptier = ptier,
-                        pkey = pkey,
-                        plev = plev,
-                        pind = pind,
-                        pnum = pnum,
-                        pwt = pwt)
+  prm_df <- prm_create_prm_df(pname = pname,
+                              pfile = pfile,
+                              pmin = pmin,
+                              pmax = pmax,
+                              pmu = pmu,
+                              psigma = psigma,
+                              pdist = pdist,
+                              ptier = ptier,
+                              pkey = pkey,
+                              plev = plev,
+                              pind = pind,
+                              pnum = pnum,
+                              pwt = pwt)
 
   return(prm_df)
 
