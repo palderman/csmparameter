dd95dfb8f6be6951c92fcc7aafbeb09a75bccec3
Phillip D. Alderman <phillip.alderman@okstate.edu>
2025-08-07 10:25:13 -0500

    Removing tidyverse dependencies from prm_get_pvar()

--- R/prm_get_pmin_pmax_pmu_psigma.R
+++ R/prm_get_pmin_pmax_pmu_psigma.R
@@ -1,25 +1,14 @@
-#'
-#' @importFrom purrr map map_dbl
-#' @importFrom stringr str_subset str_remove
-#'
 prm_get_pvar <- function(pdensity, regex){
 
-  unlist(
-    lapply(
-      lapply(
-        lapply(
-          # convert function into text
-          lapply(pdensity, deparse),
-          # subset to line in text that contanins regex
-          function(.x) grep(regex, .x, value = TRUE)
-        ),
-        # Remove variable name and assignment operator
-        function(.x) gsub(".*<-", "", .x)
-      ),
-      # Convert text value into numeric
-      function(.x) if(length(.x) == 0) NA_real_ else as.numeric(.x)
-    )
-  )
+  # convert function into text
+  lapply(pdensity, deparse) |>
+    # subset to line in text that contanins regex
+    lapply(\(.x) grep(regex, .x, value = TRUE)) |>
+    # Remove variable name and assignment operator
+    lapply(\(.x) gsub(".*<-", "", .x)) |>
+    # Convert text value into numeric
+    lapply(\(.x) if(length(.x) == 0) NA_real_ else as.numeric(.x)) |>
+    unlist()
 
 }
 
