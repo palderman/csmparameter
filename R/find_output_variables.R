2b66142b7763c300a67b0b920c3c873ff7654745
Phillip D. Alderman <phillip.alderman@okstate.edu>
2025-08-08 00:04:18 -0500

    Removing tidyverse dependencies from find_output_variables()

--- R/find_output_variables.R
+++ R/find_output_variables.R
@@ -1,23 +1,22 @@
-#' @importFrom dplyr  rowwise mutate ungroup arrange filter select
-#' @importFrom tibble tibble
-#' @importFrom purrr map map_lgl
-#' @importFrom stringr str_c str_subset
-#'
-find_output_variables <- function(.expmt){
+find_output_variables <- function(.expmt,
+                                  prioritize_files = c('Summary.OUT',
+                                                       'PlantGro.OUT')){
 
   raw_out_files <- list.files(pattern = '(\\.OUT)|(\\.csv)') |>
-    (\(.x) c('Summary.OUT', 'PlantGro.OUT', .x))() |>
+    (\(.x) c(prioritize_files, .x))() |>
     unique() |>
-    str_subset("Measured", negate = TRUE) |>
+    grep("Measured", x = _,
+         value = TRUE, invert = TRUE) |>
     (\(.x) setNames(.x,.x))() |>
     map(function(.x) if(file.exists(.x)) readLines(.x) else NULL)
 
   data_type_regex <- .expmt$data_types |>
     unlist() |>
-    (\(.x) paste0("(", .x, ")"))() |>
+    paste0("(", .x = _, ")") |>
     paste0(collapse = "|")
 
-  headers <- vector(mode = "list", length = length(raw_out_files))
+  headers <- vector(mode = "list",
+                    length = length(raw_out_files))
   for(i in seq_along(headers)){
     if(grepl("\\.csv$", names(raw_out_files)[i])){
       headers[[i]] <- raw_out_files[[i]][1]
@@ -27,27 +26,34 @@ find_output_variables <- function(.expmt){
   }
   names(headers) <- names(raw_out_files)
 
-  out_df <- headers |>
-    map_lgl(~any(str_detect(., data_type_regex))) |>
-    (\(.x) subset(raw_out_files, .x)
-     )() |>
+  out_list <-
+    headers |>
+    sapply(\(.x) any(grepl(data_type_regex, .x))) |>
+    subset(raw_out_files, subset = _) |>
     names() |>
-    (\(.x) setNames(.x, .x)
+    setNames(nm = _) |>
+    lapply(\(.x) try(suppressWarnings(read_output(.x)), silent = TRUE))
+
+  out_df <-
+    out_list |>
+    lapply(\(.x) ! "try-error" %in% class(.x) && any(colnames(.x) %in% unlist(.expmt$data_types))) |>
+    subset(out_list, subset = _) |>
+    (\(.x) data.frame(file_name = names(.x),
+                      col_names = I(lapply(.x, colnames)),
+                      data_types = I(list(unlist(.expmt$data_types))))
      )() |>
-    map(~try(suppressWarnings(read_output(.)), silent = TRUE)) |>
-    (\(.x) .x[map_lgl(.x, ~{ ! 'try-error' %in% class(.) })]
-     )() |>
-    (\(.x) .x[map_lgl(.x,~{ any(colnames(.) %in% unlist(.expmt$data_types)) })]
-     )() |>
-    (\(.x) tibble(file_name = names(.x),
-                  col_names = map(.x,colnames),
-                  data_types = list(unlist(.expmt$data_types)))
-     )() |>
-    rowwise() |>
-    mutate(dtype_check = list(intersect(data_types,col_names)),
-           nvars = length(unlist(dtype_check))) |>
-    ungroup() |>
-    arrange(nvars)
+    within({
+      dtype_check = mapply(intersect,
+                           x = data_types,
+                           y = col_names)
+      nvars = dtype_check |>
+        lapply(unlist) |>
+        lapply(length) |>
+        unlist()
+    })
+
+  out_df <-
+    out_df[order(out_df[["nvars"]]), ]
 
   for(i in 1:nrow(out_df)){
     if(i < nrow(out_df)){
@@ -59,18 +65,22 @@ find_output_variables <- function(.expmt){
     }
   }
 
-  missing_data_types <- setdiff(unlist(.expmt$data_types),unlist(out_df$col_names))
+  missing_data_types <- setdiff(unlist(.expmt$data_types),
+                                unlist(out_df$col_names))
 
   if(length(missing_data_types) > 0){
-    err_msg <- str_c(missing_data_types,collapse = ', ') |>
-      str_c('The following data types were not found in any output file:\n',
-            .,
-            '\n Please check variable names or output options within File X.')
+    err_msg <-
+      paste0(missing_data_types, collapse = ', ') |>
+      paste0('The following data types were not found in any output file:\n',
+             x = _,
+             '\n Please check variable names or output options within File X.')
     warning(err_msg)
   }
 
-  out_df <- filter(out_df,map_lgl(col_names,~{length(.) > 0})) |>
-    select(file_name,col_names)
+  out_df <-
+    out_df |>
+    subset(sapply(out_df$col_names, \(.x) length(.x) > 0),
+           select = c(file_name, col_names))
 
   return(out_df)
 
