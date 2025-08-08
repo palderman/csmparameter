72e737783931602756cbeea33d3ac3469ce576ae
Phillip D. Alderman <phillip.alderman@okstate.edu>
2025-08-07 21:08:33 -0500

    Removing tidyverse dependencies from prm_from_eco_file()

--- R/prm_from_eco_file.R
+++ R/prm_from_eco_file.R
@@ -1,29 +1,32 @@
 #'
-#' @importFrom dplyr filter select rename mutate
-#' @importFrom tidyr pivot_longer pivot_wider
-#' @importFrom DSSAT read_eco
-#'
 prm_from_eco_file <- function(file_path){
 
-  eco <- read_eco(file_path)
+  eco <- DSSAT::read_eco(file_path) |>
+    subset(`ECO#` %in% c("999991", "999992", "DFAULT"))
 
-  prm_df <- eco |>
-    filter(`ECO#` %in% c("999991", "999992", "DFAULT")) |>
-    pivot_longer(-`ECO#`) |>
-    pivot_wider(names_from = `ECO#`) |>
-    rename(pname = name,
-           pmin = `999991`,
-           pmax = `999992`,
-           pmu = DFAULT) |>
-    mutate(psigma = (pmax-pmin)/6,
-           pfile = basename(file_path)) |>
-    with(prm_create(pname = pname,
-                    pfile = pfile,
-                    pmin = pmin,
-                    pmax = pmax,
-                    pmu = pmu,
-                    psigma = psigma,
-                    pdist = "normal"))
+  eco_df <-
+    eco |>
+    subset(select = -`ECO#`) |>
+    t() |>
+    as.data.frame() |>
+    setNames(eco[["ECO#"]]) |>
+    df_rename(pmin = "999991",
+              pmax = "999992",
+              pmu = "DFAULT")
 
-  return(prm_df)
+  eco_df[["pname"]] <- rownames(eco_df)
+
+  eco_df |>
+    within({
+      psigma = (pmax-pmin)/4
+      pfile = basename(file_path)
+    }) |>
+    with(prm_create_prm_df(pname = pname,
+                           pfile = pfile,
+                           pmin = pmin,
+                           pmax = pmax,
+                           pmu = pmu,
+                           psigma = psigma,
+                           pdist = "normal"))
+
 }
