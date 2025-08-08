97124b0af5bafacbc7d2e7ceb25715c99c30ce4e
Phillip D. Alderman <phillip.alderman@okstate.edu>
2025-08-07 21:18:11 -0500

    Removing tidyverse dependencies from prm_from_cul_file()

--- R/prm_from_cul_file.R
+++ R/prm_from_cul_file.R
@@ -1,30 +1,34 @@
 #'
-#' @importFrom dplyr filter select rename mutate
-#' @importFrom tidyr pivot_longer pivot_wider
-#' @importFrom DSSAT read_cul
-#'
 prm_from_cul_file <- function(file_path){
 
-  cul <- read_cul(file_path)
+  cul <- DSSAT::read_cul(file_path) |>
+    subset(`VAR-NAME` %in% c("MINIMA", "MAXIMA", "DEFAULT"),
+           select = -c(`VAR#`, `EXP#`, `ECO#`))
 
-  prm_df <- cul |>
-    filter(`VAR-NAME` %in% c("MINIMA", "MAXIMA", "DEFAULT")) |>
-    select(-`VAR#`, -`EXP#`, -`ECO#`) |>
-    pivot_longer(-`VAR-NAME`) |>
-    pivot_wider(names_from = `VAR-NAME`) |>
-    rename(pname = name,
-           pmin = MINIMA,
-           pmax = MAXIMA,
-           pmu = DEFAULT) |>
-    mutate(psigma = (pmax-pmin)/6,
-           pfile = basename(file_path)) |>
-    with(prm_create(pname = pname,
-                    pfile = pfile,
-                    pmin = pmin,
-                    pmax = pmax,
-                    pmu = pmu,
-                    psigma = psigma,
-                    pdist = "normal"))
+  cul_df <-
+    cul |>
+    subset(select = -`VAR-NAME`) |>
+    t() |>
+    as.data.frame() |>
+    setNames(cul[["VAR-NAME"]]) |>
+    df_rename(pmin = "MINIMA",
+              pmax = "MAXIMA",
+              pmu = "DFAULT")
 
-    return(prm_df)
+  cul_df[["pname"]] <- rownames(cul_df)
+
+
+  cul_df |>
+    within({
+      psigma = (pmax-pmin)/4
+      pfile = basename(file_path)
+    }) |>
+    with(prm_create_prm_df(pname = pname,
+                           pfile = pfile,
+                           pmin = pmin,
+                           pmax = pmax,
+                           pmu = pmu,
+                           psigma = psigma,
+                           pdist = "normal"))
+
 }
